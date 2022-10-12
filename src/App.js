732a725a681b3698894b335d2e24943dc581b3b4
junaaid96 <info.junaidulislam@gmail.com>
2022-10-13 00:00:37 +0600

    routes added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Blog from './Components/Blog/Blog';
+import Statistics from './Components/Statistics/Statistics';
 import Topic from './Components/Topic/Topic';
 import Main from './Layout/Main';
 
 function App() {
-const router = createBrowserRouter([
-  {
-    path: '/', 
-    element: <Main></Main>,
-    children: [
-      {
-        path: '/',
-        loader: async() => {
-         return fetch('https://openapi.programming-hero.com/api/quiz')
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          loader: async () => {
+            return fetch('https://openapi.programming-hero.com/api/quiz')
+          },
+          element: <Topic></Topic>
         },
-        element: <Topic></Topic>
-      }
-    ]
-  }
-]);
+        {
+          path: 'statistics',
+          element: <Statistics></Statistics>
+        },
+        {
+          path: 'blog',
+          element: <Blog></Blog>
+        }
+      ]
+    },
+    
+  ]);
 
   return (
     <div className="App">
