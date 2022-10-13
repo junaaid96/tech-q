2711bd96e69c10656c10a48d47c4c8927506b1f5
junaaid96 <info.junaidulislam@gmail.com>
2022-10-13 20:42:19 +0600

    chart made responsive

--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,32 +1,32 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from "recharts";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
 
 const Statistics = () => {
     const topics = useLoaderData();
     const topic = topics.data.map(topic => topic);
 
     return (
-        <Container fluid>
-            <h1 className='mb-5'>Questions Graph</h1>
-            <BarChart
-                className='m-auto'
-                width={1000}
-                height={500}
-                data={topic}
-                margin={{
-                    top: 5, right: 30, left: 20, bottom: 5
-                }}
-            >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Bar dataKey="total" fill="#82ca9d" />
-            </BarChart>
-        </Container>
+        <>
+            <h1>Questions Graph</h1>
+            <ResponsiveContainer className='m-auto mt-5' height={500} width="80%">
+                <BarChart
+                    // width={1000}
+                    // height={500}
+                    data={topic}
+                    margin={{
+                        top: 5, right: 30, left: 20, bottom: 5
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="total" fill="#82ca9d" />
+                </BarChart>
+            </ResponsiveContainer>
+        </>
     );
 };
 
